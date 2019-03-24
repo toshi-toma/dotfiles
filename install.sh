026a7f4da63a26723d58a9b768b78bf8e49e80b3
toshi-toma <toshitoma1217@gmail.com>
2019-03-24 14:14:19 +0900

    add install script

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-#TBD
+#!/bin/sh -xe
+
+# 実行場所のディレクトリを取得
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+cd $THIS_DIR
+
+echo "\033[0;32mstart setup... \033[0;39m"
+
+# dotfileのシンボリックリンク作成
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    ln -snfv ~/dotfiles/"$f" ~/
+done
+
+echo "\033[0;32mFinish!!! \033[0;39m"
